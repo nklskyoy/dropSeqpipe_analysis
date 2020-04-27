331e368e8d720ab6f689f3c1b79a9a293b8a1c4a
o <o.nikolskyy@meanware.de>
2020-04-27 22:21:29 +0200

    add test mmap

--- merge_and_repair/dataset_small/barplot.py
+++ merge_and_repair/dataset_small/barplot.py
@@ -1,6 +1,7 @@
 import os
 import matplotlib.pyplot as plt
-
+import matplotlib.patches as mpatches
+import numpy as np
 samples = [["255_S4", "254_S3"], ["253_S2", "252_S1"]]
 
 rules_original = ["get_top_barcodes", "MergeBamAlignment","repair_barcodes"]
@@ -11,24 +12,55 @@ def read_sec_from_file(path):
     file  = open(path,"r")
     haeder = file.readline()
     benchmarks = file.readline()
-    sec = benchmarks.split("\t")[0]
+    sec = float(benchmarks.split("\t")[0])
     file.close()
     return sec
 
 
-path_original = os.getcwd()+"/original/"
-path_accelerate = os.getcwd()+"/accelerate/"
+path_original = os.getcwd()+"/"+os.path.dirname(__file__)+"/original/"
+path_accelerate = os.getcwd()+"/"+os.path.dirname(__file__)+"/accelerate/"
 
 print(path_original)
 print(path_accelerate)
+
+
+get_top_barcodes_patch = mpatches.Patch(color='indianred', hatch="+",label='get_top_barcodes')
+MergeBamAlignment_patch = mpatches.Patch(color='grey', hatch=".",label="MergeBamAlignment")
+repair_barcodes_patch = mpatches.Patch(color='orchid', hatch="\\",label="repair_barcodes")
+
+acc_patch = mpatches.Patch(color='lightblue', hatch="//",label="merge_and_repair")
 
 fig,axs = plt.subplots(2,2)
 for i in range(len(samples)):
     for j in range(len(samples[i])):
         sample = samples[i][j]
-        sec_acc = read_sec_from_file(path_accelerate+"MERGE_AND_REPAIR."+sample+".txt")
+        sec_acc = [read_sec_from_file(path_accelerate+"MERGE_AND_REPAIR."+sample+".txt"),0,0]
 
         sec_original = [ read_sec_from_file(path_original+rules_original[r]+"."+sample+".txt")
             for r in range(len(rules_original))]
 
-        print(sec_original)
+        sec = np.array([sec_acc,sec_original]).transpose()
+        print(sec[0])
+        lev1 = axs[i,j].bar([1,2],sec[0],1,color=["lightblue","indianred"])
+        lev2 = axs[i,j].bar([1,2],sec[1],1,bottom=sec[0], color=["lightblue","grey"])
+        lev3 = axs[i,j].bar([1,2],sec[2],1,bottom=sec[1]+sec[0], color=["lightblue","orchid"])
+
+        lev1[0].set_hatch("\\") # accelerated bar
+        lev1[1].set_hatch("+")
+
+        lev2[1].set_hatch(".")     # die oberste bar
+
+        lev3[1].set_hatch("//")
+
+
+
+        print(sec[0])
+        print(sec[1])
+
+for ax in fig.get_axes():
+    ax.label_outer()
+    ax.set_xticklabels(["",""])
+
+fig.legend(handles=[get_top_barcodes_patch,MergeBamAlignment_patch,repair_barcodes_patch,acc_patch])
+
+plt.show()
---
+++ test_mmap_vs_read.py
@@ -0,0 +1,41 @@
+import sys
+import functools
+import os
+import mmap
+import numpy as np
+import matplotlib.pyplot as plt
+import gzip
+import timeit
+
+time_mmap = []
+reads_mmap = []
+
+time_read = []
+reads_read = []
+
+time_seqio = []
+
+
+def _read(file,n_reads):
+    ctr = 0
+    while ctr < n_reads*4:
+        file.readline()
+
+
+gz = gzip.open(str(sys.argv)[1],"r")
+
+for i in range(4* 5* 10*6):
+    t = timeit.Timer(functools.partial(_read,gz,i))
+    time_read.append(t)
+    reads_read.append(i)
+
+mfd = os.open('BigFile', os.O_RDONLY)
+mfile = mmap.mmap(mfd, 0, prot=mmap.PROT_READ)
+
+for i in range(4* 5* 10*4):
+    t = timeit.Timer(functools.partial(_read,mfd,i))
+    time_mmap.append(t)
+    reads_mmap.append(i)
+
+print(time_read)
+print(time_mmap)
