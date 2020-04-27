8eeb5e0fae0f7a462e8538b14a6eadfb33435141
o <o.nikolskyy@meanware.de>
2020-04-27 23:32:47 +0200

    loop limit

--- test_mmap_vs_read.py
+++ test_mmap_vs_read.py
@@ -24,15 +24,17 @@ def _read(file,n_reads):
 
 gz = gzip.open(sys.argv[1],"r")
 
-for i in range(4* 10*3):
+for i in range(4* 10*2):
     t = timeit.Timer(functools.partial(_read,gz,i)).timeit(1)
     time_read.append(t)
     reads_read.append(i)
 
+print("finished benchmarking 1")
+
 mfd = os.open(sys.argv[1], os.O_RDONLY)
 mfile = mmap.mmap(mfd, 0, prot=mmap.PROT_READ)
 
-for i in range(4* 5* 10*3):
+for i in range(4* 5* 10*2):
     t = timeit.Timer(functools.partial(_read,mfd,i)).timeit(1)
     time_mmap.append(t)
     reads_mmap.append(i)
