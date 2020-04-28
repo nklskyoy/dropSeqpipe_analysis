3a92a915f8af3fb5112af8dac7aeabbdcc1fbc7a
o <o.nikolskyy@meanware.de>
2020-04-28 08:57:36 +0200

    save results of test

--- test_mmap_vs_read.py
+++ test_mmap_vs_read.py
@@ -14,9 +14,9 @@ time_read = []
 reads_read = []
 
 time_seqio = []
+reads_seqio = []
 
-
-TOT_READS = 10**4
+TOT_READS = 10**5
 
 def _read(file,n_reads):
     ctr = 0
@@ -29,8 +29,9 @@ gz = gzip.open(sys.argv[1],"r")
 
 for i in range(4*TOT_READS):
     t = timeit.Timer(functools.partial(_read,gz,i)).timeit(1)
-    time_read.append(t)
-    reads_read.append(i)
+    if i % 4 == 0:
+        time_read.append(t)
+        reads_read.append(i)
 
 print("finished benchmarking 1")
 
@@ -39,12 +40,19 @@ mfile = mmap.mmap(mfd, 0, prot=mmap.PROT_READ)
 
 for i in range(4*TOT_READS):
     t = timeit.Timer(functools.partial(_read,mfile,i)).timeit(1)
-    time_mmap.append(t)
-    reads_mmap.append(i)
+    if i % 4 == 0:
+        time_mmap.append(t)
+        reads_mmap.append(i)
 
 print(time_read)
 print(time_mmap)
 
-plt.plot(reads_mmap,time_mmap,'.')
-plt.plot(reads_read,time_read,'.')
-plt.savefig("mmapvsread.png")
+np.save("time_read.npy",time_read)
+np.save("time_mmap.npy",time_mmap)
+np.save("reads_read.npy",reads_read)
+np.save("reads_mmap.npy",reads_mmap)
+
+
+#plt.plot(reads_mmap,time_mmap,'.')
+#plt.plot(reads_read,time_read,'.')
+#plt.savefig("mmapvsread.png")
